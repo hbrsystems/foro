e3ef0f519ab8527a77892bdfa266eed386e4657b
hbrsystems <66086285+hbrsystems@users.noreply.github.com>
2020-06-22 11:02:14 -0500

    first commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ExampleTest extends TestCase {
+	/**
+	 * A basic functional test example.
+	 *
+	 * @return void
+	 */
+
+//	use DatabaseMigrations;
+	use DatabaseTransactions;
+	public function testBasicExample() {
+
+		//crear usuario
+		$name = 'HENRY BARON';
+		$email = 'admin@styde.net';
+		$user = factory(\App\User::class)->create([
+			'name' => $name,
+			'email' => $email]);
+		//conectar usuario
+		$this->actingAs($user, 'api');
+		$this->visit('api/user')
+			->see($name)
+			->see($email);
+	}
+}
